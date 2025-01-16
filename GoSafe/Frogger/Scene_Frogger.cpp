5dbbfca813a1bc3d865e7930fdfeec9b27e34248
RUDRAPATEL19 <rpatel19@mynbcc.ca>
2025-01-15 22:21:49 -0400

    Game Engine setup

---
+++ GoSafe/Frogger/Scene_Frogger.cpp
@@ -0,0 +1,27 @@
+#include "Scene_Frogger.h"
+//
+// Created by David Burchill on 2023-09-27.
+//
+
+
+Scene_Frogger::Scene_Frogger(GameEngine* gameEngine, const std::string& levelPath)
+    : Scene(gameEngine)
+{   
+}
+
+
+void Scene_Frogger::onEnd()
+{
+}
+
+void Scene_Frogger::update(sf::Time dt)
+{
+}
+
+void Scene_Frogger::sDoAction(const Command& action)
+{
+}
+
+void Scene_Frogger::sRender()
+{
+}
